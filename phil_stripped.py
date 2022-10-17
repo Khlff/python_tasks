09aec5c4ccb34be4f8412c894373dccbea6cd2eb
khlff <n.hlopunov@ya.ru>
2022-10-17 20:11:35 +0500

    upd

---
+++ phil_stripped.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import re
+from urllib import request, parse, error
+
+
+def get_content(name):
+    """
+    Функция возвращает содержимое вики-страницы name из русской Википедии.
+    В случае ошибки загрузки или отсутствия страницы возвращается None.
+    """
+    try:
+        url = 'https://ru.wikipedia.org/wiki/' + parse.quote(name)
+        result = request.urlopen(url)
+        return result.read().decode(result.headers.get_content_charset())
+    except error.HTTPError:
+        return None
+    except error.URLError:
+        return None
+
+
+def extract_content(page):
+    """
+    Функция принимает на вход содержимое страницы и возвращает 2-элементный
+    tuple, первый элемент которого — номер позиции, с которой начинается
+    содержимое статьи, второй элемент — номер позиции, на котором заканчивается
+    содержимое статьи.
+    Если содержимое отсутствует, возвращается (0, 0).
+    """
+
+    if page is None:
+        return (0, 0)
+    # print(re.findall(page, r'\\class="vector-body">.*?\\<div id="mw-navigation">'))
+    first_index = page.find('class="vector-body">')
+    second_index = page.find('<div id="mw-navigation">')
+    return page[first_index:second_index]
+
+
+print(extract_content(get_content('Ящерицы')))
+
+
+def extract_links(page, begin, end):
+    """
+    Функция принимает на вход содержимое страницы и начало и конец интервала,
+    задающего позицию содержимого статьи на странице и возвращает все имеющиеся
+    ссылки на другие вики-страницы без повторений и с учётом регистра.
+    """
+    pass
+
+
+def find_chain(start, finish):
+    """
+    Функция принимает на вход название начальной и конечной статьи и возвращает
+    список переходов, позволяющий добраться из начальной статьи в конечную.
+    Первым элементом результата должен быть start, последним — finish.
+    Если построить переходы невозможно, возвращается None.
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
