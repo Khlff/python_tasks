571304fe9750c6a8ccfca13fdbeb97bf3c7122f5
khlff <n.hlopunov@ya.ru>
2022-10-25 13:07:09 +0500

    upd

--- phil_stripped.py
+++ phil_stripped.py
@@ -1,4 +1,4 @@
-import datetime
+import sys
 import re
 from urllib import request, parse, error
 
@@ -13,8 +13,10 @@ def get_content(name):
         result = request.urlopen(url)
         return result.read().decode(
             result.headers.get_content_charset()).replace('\n', '')
+
     except error.HTTPError:
         return None
+
     except error.URLError:
         return None
 
@@ -31,11 +33,11 @@ def extract_content(page):
     if page is None:
         return (0, 0)
 
-    result_of_search = \
+    result_of_search = (
         re.search(
             r'<div id="content" class="mw-body" role="main">(.*)</div><div '
             r'id="mw-navigation">',
-            page)
+            page))
 
     return (result_of_search.start(),
             result_of_search.end()) if result_of_search is not None else (0, 0)
@@ -47,24 +49,30 @@ def extract_links(page, begin, end):
     задающего позицию содержимого статьи на странице и возвращает все имеющиеся
     ссылки на другие вики-страницы без повторений и с учётом регистра.
     """
-    links_list = re.findall(r'href="/wiki/([\w%\(\)\_]*)"',
-                            page[begin:end])  # тут срёт из-за %, . и тд
+
+    links_list = re.findall(
+        r'[hH][rR][eE][fF]=[\'\"]/wiki/([\w%\(\)\_\,]*)[\'\"]',
+        page[begin:end])
+
     normalized_links = []
     for link in links_list:
         link = parse.unquote(link)
         if link is not None and link not in normalized_links:
             normalized_links.append(link)
+
     return normalized_links
 
 
-def get_name_of_article(content):
-    if content == None:
-        names = None
-    else:
-        names = re.findall(r'<span class="mw-page-title-main">(.*?)</span>',
-                           content)
-    name = names[0].replace(' ', '_') if names else None
-    return name
+def find_way(dict_of_sites_names, what_to_find):
+    list_of_path = [what_to_find]
+    temp_site = what_to_find
+
+    while temp_site in dict_of_sites_names.keys():
+        temp_site = dict_of_sites_names[temp_site]
+        list_of_path.append(temp_site)
+
+    list_of_path.reverse()
+    return list_of_path
 
 
 def find_chain(start, finish):
@@ -79,48 +87,40 @@ def find_chain(start, finish):
         return [start]
 
     start = start.replace(' ', '_')
-    finish = start.replace(' ', '_')
+    finish = finish.replace(' ', '_')
 
     list_of_used_links = [start]
-    table_of_links = {start: []}
-    queue = []
-
-    content_of_start_page = get_content(start)
-    start_of_content, end_of_content = extract_content(content_of_start_page)
-    links_of_start_page = extract_links(content_of_start_page,
-                                        start_of_content, end_of_content)
-
-    for link in links_of_start_page:
-        if link not in list_of_used_links:
-            table_of_links[start].append(link)
-            list_of_used_links.append(link)
-            queue.append(link)
-        if link == finish:
-            return table_of_links
+    table_of_links = {}
+    queue = [start]
 
     while queue:
         link_from_queue = queue.pop(0)
         content_of_queue_page = get_content(link_from_queue)
+        if content_of_queue_page is None:
+            continue
         start_of_content, end_of_content = extract_content(
             content_of_queue_page)
         links_of_queue_page = extract_links(content_of_queue_page,
                                             start_of_content, end_of_content)
 
-        table_of_links[link_from_queue] = []
         for link in links_of_queue_page:
             if link not in list_of_used_links:
                 queue.append(link)
-                table_of_links[link_from_queue].append(link)
+                table_of_links[link] = link_from_queue
                 list_of_used_links.append(link)
             if link == finish:
-                return table_of_links
+                return find_way(table_of_links, finish)
 
 
 def main():
-    first = datetime.datetime.now()
-    print(find_chain('Путин,_Владимир_Владимирович', 'Президент'))
-    second = datetime.datetime.now()
-    print(second-first)
+    args = sys.argv
+    if len(args) == 3:
+        start_point = args[1]
+        end_point = args[2]
+    else:
+        return None
+    print(find_chain(start_point, end_point))
+
 
 if __name__ == '__main__':
     main()
